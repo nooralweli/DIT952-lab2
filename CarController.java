4589531aec3612921b647f7480c85d53c37a0b90
nooralweli <noor_alwali@hotmail.com>
2018-06-19 16:52:23 +0200

    Update CarController.java

--- CarController.java
+++ CarController.java
@@ -24,7 +24,7 @@ public class CarController {
     ArrayList<ACar> cars = new ArrayList<>();
 
     //methods:
-    //hej
+    //hej hej
 
     public static void main(String[] args) {
         // Instance of this class
